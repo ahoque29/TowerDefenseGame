5c8288d8da7d627ecd552203ec768f38fd7b3bc7
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-01 22:04:53 +0000

    made constructor for map class

--- TowerDefense/Map.cs
+++ TowerDefense/Map.cs
@@ -2,6 +2,26 @@
 {
 	class Map
 	{
+		// Map class needs variable of width and height.
+		// We need a constructor method for height and width to make sure those are filled when a map instance is created.
 
+		public readonly int Width;
+		public readonly int Height;
+		// We declared these two fields to be used for the Map class.
+		// Every map object that instantiated can store both width and height.
+		// Keyword public/private/protected define the access level
+		// public can be accessed anywhere. private cannot be accessed anywhere but here. protected can be accessed by subclasses.
+		// readonly keyword used to ensure values are not changed after declaration.
+
+		public Map(int width, int height) // Constuctors have the same name as the class they are in. Parameters added in the brackets.
+		{
+			// Parameters added must be provided by the user.
+			// Body of the constructor used to initialise the fields.
+			Width = width;
+			Height = height;
+
+			// Constructors do not have return types.
+			// public keyword used so that other classes can use this Constructor.
+		}
 	}
 }
--- TowerDefense/Program.cs
+++ TowerDefense/Program.cs
@@ -4,9 +4,11 @@ namespace TowerDefense
 {
 	class Program //Program that will hold main.
 	{
-		public void Main(string[] args)
+		public void Main()
 		{
-			Tower tower = new Tower(); //This makes a new variable of the Tower type called tower. notice the variable name being lower case.
+			Map map = new Map(8, 5);
+			// Making new instances of map. After the constructor has been made, fields to be instantiated in the brackets.
+
 		}
 	}
 }
