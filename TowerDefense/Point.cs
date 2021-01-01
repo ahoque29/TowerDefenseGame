18d1bbb9dbc9fbc903312c41707dd4ef951e9a13
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-01 23:33:10 +0000

    created a distanceto method for point and another method distanceto with different parameters (overload)

--- TowerDefense/Point.cs
+++ TowerDefense/Point.cs
@@ -1,4 +1,6 @@
-﻿namespace TowerDefense
+﻿using System;
+
+namespace TowerDefense
 {
 	class Point //Point class will need fields for x and y coordinate.
 	{
@@ -11,6 +13,23 @@
 			Y = y;
 		}
 
+		public int DistanceTo(int x, int y)
+		{
+			// Method to determine distance between two points.
+			// This is to be used on a point variable and parameters x and y coordinates need to be passed.
+			
+			return (int)Math.Sqrt(Math.Pow(X - x, 2) + Math.Pow(Y - y, 2));
+			// Math.Sqrt used to computer square root.
+			// Math.Pow used to computer exponentiation.
+			// Method requires to return an integer so the result is casted to (int). This truncates the double after the decimal point.
+		}
 
+		public int DistanceTo(Point point)
+		{
+			// Now we make another method with the same name that takes different parameters, this time a point.
+
+			return DistanceTo(point.X, point.Y);
+			// Reused the DistanceTo method but passed the x and y coordiate of the point instead. This is called Overloading a method.
+		}
 	}
 }
--- TowerDefense/Program.cs
+++ TowerDefense/Program.cs
@@ -4,15 +4,22 @@ namespace TowerDefense
 {
 	class Program //Program that will hold main.
 	{
-		public void Main()
+		static void Main()
 		{
 			Map map = new Map(8, 5);
 			// Making new instances of map. After the constructor has been made, fields to be instantiated in the brackets.
 
-			Point point = new Point(4, 2);
+			Point point1 = new Point(4, 2);
+			Point point2 = new Point(2, 3);
 
-			bool isOnMap = map.OnMap(point);
-			// A Bool variable is made by using the OnMap() method on the variable map by with parameter point.
+			bool isOnMap = map.OnMap(point1);
+			// A Bool variable is made by using the OnMap() method on the variable map by with parameter point1.
+
+			Console.WriteLine(point1.DistanceTo(5, 5));
+			// Prints the distance from point to (5, 5) as an integer to the console.
+
+			Console.WriteLine(point1.DistanceTo(point2));
+			// Prints the distance from point to point2 as an integer to the console using the overloaded method.
 		}
 	}
 }
