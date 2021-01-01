5c3c902259d4942641c33e86c341ddf68eee8228
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-01 21:24:35 +0000

    Initial commit: made the main, tower, invader, map and path classes

---
+++ TowerDefense/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TowerDefense
+{
+	class Program //Program that will hold main.
+	{
+		public void Main(string[] args)
+		{
+			Tower tower = new Tower(); //This makes a new variable of the Tower type called tower. notice the variable name being lower case.
+		}
+	}
+}
---
+++ TowerDefense/Tower.cs
@@ -0,0 +1,7 @@
+﻿namespace TowerDefense //This is the namespace that all classes are under.
+{
+	class Tower //Class declared called Tower. Classes are conventionally names starting with upper case.
+	{
+
+	}
+}
