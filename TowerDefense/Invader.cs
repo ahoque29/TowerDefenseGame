3d418d7d9d87954bdd074ce180c081b06a228ae3
Ashib Hoque <gaffingsiv@hotmail.com>
2021-01-03 02:52:41 +0000

    Initiated the Invader class with a MapLocation field. Made two methods in it, one to get its location and one to set it.

--- TowerDefense/Invader.cs
+++ TowerDefense/Invader.cs
@@ -2,6 +2,24 @@
 {
 	class Invader 
 	{
+		// Invader needs a MapLocation to see how close it is to a Tower if they are in range of each other.
+		// It shouldn't be readonly because the Invader should be moving through the path.
+		// The method should not be public because this divulges too much info.
+		// Instead, make the field private and include public methods to access the field.		
+		private MapLocation _location;
 
+		// Need two methods. One that accesses the location and one that sets the location.
+		// Make use of accessor methods.
+		// Method to get the location:
+		public MapLocation GetLocation()
+		{
+			return _location;
+		}
+
+		// And now to set the location:
+		public void SetLocation(MapLocation value)
+		{
+			_location = value;
+		}
 	}
 }
